091e206ba5562c67eb9f7fcc0c2f3488d004cf4a
gabdias <gabdias1609@gmail.com>
2020-06-16 11:56:40 -0300

    End of section 3

--- WebAPICodeUdemy3/WebAPICodeUdemy3/Data/DataContext.cs
+++ WebAPICodeUdemy3/WebAPICodeUdemy3/Data/DataContext.cs
@@ -9,5 +9,7 @@ namespace WebAPICodeUdemy3.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
 
         public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ WebAPICodeUdemy3/WebAPICodeUdemy3/Dtos/UserForLoginDto.cs
@@ -0,0 +1,10 @@
+namespace WebAPICodeUdemy3.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
